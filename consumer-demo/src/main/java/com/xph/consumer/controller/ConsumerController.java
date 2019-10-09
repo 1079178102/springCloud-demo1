16fbf1191747ca1b1c00dbb12a857fd41130037c
xupenghui_pro <xph@qq.com>
2019-10-09 11:19:05 +0800

    使用discoveryClient动态获取user-service服务的Host(ip)与Port(端口)

--- consumer-demo/src/main/java/com/xph/consumer/controller/ConsumerController.java
+++ consumer-demo/src/main/java/com/xph/consumer/controller/ConsumerController.java
@@ -2,11 +2,15 @@ package com.xph.consumer.controller;
 
 import com.xph.consumer.pojo.User;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cloud.client.ServiceInstance;
+import org.springframework.cloud.client.discovery.DiscoveryClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("consumer")
@@ -15,9 +19,15 @@ public class ConsumerController {
     @Autowired
     private RestTemplate restTemplate;
 
+    @Autowired
+    private DiscoveryClient discoveryClient;
+
     @GetMapping("{id}")
     public User queryByid(@PathVariable("id")Long id){
-        String url = "http://localhost:8081/user/"+id;
+        List<ServiceInstance> instances = discoveryClient.getInstances("user-service");
+        ServiceInstance instance = instances.get(0);
+        String url = "http://" + instance.getHost() + ":" + instance.getPort() + "/user/"+id;
+        System.out.println(url);
         User user = restTemplate.getForObject(url,User.class);
         return user;
     }
