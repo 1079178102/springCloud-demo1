ba58b21013236b7624409da4a9e99394fb430f8a
xupenghui_pro <xph@qq.com>
2019-10-09 10:57:26 +0800

    将user-service与cunsumer-server注册到eureka-server上

--- user-service/src/main/java/com/xph/UserApplication.java
+++ user-service/src/main/java/com/xph/UserApplication.java
@@ -2,8 +2,12 @@ package com.xph;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import tk.mybatis.spring.annotation.MapperScan;
 
+
+@EnableDiscoveryClient //能注册到3种服务上，@EnableEurekaClient只能注册到eureka上
 @SpringBootApplication
 @MapperScan("com.xph.user.mapper")
 public class UserApplication {
